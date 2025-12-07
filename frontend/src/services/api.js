0bc48fcfada71d9a3607a4386c2ec13639b957ef
rakshakpatre <rakshakpatre409@gmail.com>
2025-12-07 19:46:04 +0530

    add api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL|| 'http://localhost:5000/api';
+const API_BASE_URL =  'http://localhost:5000/api';
 
 export async function fetchHistory() {
   const res = await axios.get(`${API_BASE_URL}/history`);
@@ -8,7 +8,7 @@ export async function fetchHistory() {
 }
 
 export async function sendMessage(message) {
-  const res = await axios.post(`${API_BASE_URL}/chat`, { message });
+  const res = await axios.post(`${API_BASE_URL}/message`, { message });
   return res.data.messages;
 }
 
